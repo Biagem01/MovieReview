96252bf33d2433c9cd0889a3cc00f4976abf3c0a
Biagem01 <biagio.99cubisino@gmail.com>
2025-08-19 00:09:11 +0200

    Fix route order and remove merge markers

--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -3,28 +3,31 @@ const MovieController = require('../controllers/movieController');
 
 const router = express.Router();
 
+// Route generiche
 router.get('/home', MovieController.getHomeData);
 router.get('/popular', MovieController.getPopularMovies);
 router.get('/popular-tv', MovieController.getPopularTVShows);
 router.get('/search', MovieController.searchMovies);
 router.get('/search-tv', MovieController.searchTVShows);
-router.get('/search-multi', MovieController.searchMulti); // <--- AGGIUNTO QUI
+router.get('/search-multi', MovieController.searchMulti);
 router.get('/genres', MovieController.getGenres);
 router.get('/now-playing', MovieController.getNowPlaying);
 router.get('/top-rated', MovieController.getTopRated);
 router.get('/upcoming', MovieController.getUpcoming);
-router.get('/:type/:id/credits', MovieController.getCredits);
-router.get('/:type/:id/videos', MovieController.getVideos);
-router.get('/:type/:id/similar', MovieController.getSimilar);
-router.get('/movies/top-rated', MovieController.getTopRated);
-router.get('/tv/top-rated', MovieController.getTopRatedTV);
 router.get('/trending', MovieController.getTrendingMovies);
 router.get('/trending-tv', MovieController.getTrendingTVShows);
 router.get('/on-air', MovieController.getOnAirTVShows);
-router.get('/tv/:id/season/:seasonNumber', MovieController.getSeasonDetails);
+router.get('/movies/top-rated', MovieController.getTopRated);
+router.get('/tv/top-rated', MovieController.getTopRatedTV);
+
+// Route specifiche TV
 router.get('/tv/:id/season/:seasonNumber/episode/:episodeNumber', MovieController.getEpisodeDetails);
-router.get('/:type/:id', MovieController.getDetails); // <-- SEMPRE ALLA FINE
+router.get('/tv/:id/season/:seasonNumber', MovieController.getSeasonDetails);
 
-
+// Route con parametri dinamici (SEMPRE ALLA FINE)
+router.get('/:type/:id/credits', MovieController.getCredits);
+router.get('/:type/:id/videos', MovieController.getVideos);
+router.get('/:type/:id/similar', MovieController.getSimilar);
+router.get('/:type/:id', MovieController.getDetails);
 
 module.exports = router;
